773188bcb94beafcd5a1ab331ba12efd09c3a05a
JeongsangRyu <jeongsangryu@gmail.com>
2024-12-01 16:10:48 +0900

    update

--- config/only_odom.lua
+++ config/only_odom.lua
@@ -12,8 +12,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- /* Author: Darby Lim */
-
 include "map_builder.lua"
 include "trajectory_builder.lua"
 
@@ -24,54 +22,35 @@ options = {
   tracking_frame = "base_link",
   published_frame = "base_link",
   odom_frame = "odom",
-  provide_odom_frame = false,
-  publish_frame_projected_to_2d = true,
+  provide_odom_frame = true,
+  publish_frame_projected_to_2d = false,
+  use_pose_extrapolator = true,
   use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
+  publish_tracked_pose = true,
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 10, --lidar 느리고, 차량속도 빠를때 올리기
+  num_subdivisions_per_laser_scan = 10,
   num_point_clouds = 0,
-  lookup_transform_timeout_sec = 0.2,  -- 기본값 0.2초에서 증가
-  submap_publish_period_sec = 0.3,
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 1e9,  -- 서브맵 업데이트를 비활성화 (큰 값 설정)
+
+  pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
-  fixed_frame_pose_sampling_ratio = 1.,
+  fixed_frame_pose_sampling_ratio = 0.0,  -- 글로벌 위치 추정 비활성화
+
   imu_sampling_ratio = 1.,
   landmarks_sampling_ratio = 1.,
-  publish_tracked_pose = true, 
-  pose_publish_period_sec = 1e-2,
-
 }
 
-MAP_BUILDER.use_trajectory_builder_2d = true -- for 2d slam or localization
-MAP_BUILDER.num_background_threads = 6 -- Multi-threading으로 성능 최적화
-
-TRAJECTORY_BUILDER_2D.min_range = 0.12
-TRAJECTORY_BUILDER_2D.max_range = 10.
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
-TRAJECTORY_BUILDER_2D.use_imu_data = true
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
--- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5 --0.01
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 0.1 --25
+MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
-
-POSE_GRAPH.constraint_builder.min_score = 0.80
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.80
-
-TRAJECTORY_BUILDER.pure_localization_trimmer = {
-  max_submaps_to_keep = 3,
-}
-POSE_GRAPH.optimize_every_n_nodes = 10 --매핑에 비해 낮춰줘야함
-
-POSE_GRAPH.global_sampling_ratio = 0.01 -- 0.003 -- 전역 매칭 샘플링 비율
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3. -- 선형 검색 범위 (m)
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(30.) -- 각도 검색 범위 (deg)
-
--- Localization 전용으로 설정
--- POSE_GRAPH.global_constraint_search_after_n_seconds = 10. -- 전역 제약 조건 검색 주기
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false  -- 온라인 매칭 비활성화
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 1e9  -- 매우 큰 값으로 설정하여 서브맵이 거의 업데이트되지 않도록 함
 
 return options
